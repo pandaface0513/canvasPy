44bf81c6f5df59a454996d7a4e1e439a52325c39
Henry Lo <risenhell@gmail.com>
2014-11-18 11:13:54 -0800

    not sure what changed

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
+# import libraries
 import canvaslms.api as api
 import json
 
 import pymongo
+
+import os, os.path
+from whoosh import index
+from whoosh.fields import *
+from whoosh.qparser import QueryParser
+
+# set up the schema for the document
+schema = Schema(nid=TEXT(stored=True), content=TEXT(stored=True))
+
+# create index dir if it does not exists
+if not os.path.exists('indexdir'):
+    os.mkdir('indexdir')
+
+# initiate index
+ix = index.create_in('indexdir', schema)
+
+# set up tags
+tags = ['blue pill', 'education', 'secondary school', 'academy', 'influence', 'university', 'skill', 'online education', 'learning', 'tactic', 'analysis']
 
 # set up the mongoDB
 client = pymongo.MongoClient('localhost', 27017)
@@ -41,13 +60,37 @@ def clearData():
     discussion.drop()
     print('done dropping')
 
-def readingData():
+def readingData(UID):
     # then tries to get the data back
-    for entry in discussion.find({"user_id": 100724}):
+    for entry in discussion.find({"user_id": UID}):
         print(entry['message'])
 
     # declare completion!
     print('done reading')
 
-
+def writingData(UID):
+    # make a writer
+    writer = ix.writer()
+    # fill index with entries from DB
+    for entry in discussion.find({"user_id": UID}):
+        writer.update_document(nid=str(entry['id']),
+                               content=entry["message"])
+    # commit the document
+    writer.commit()
+    
+def searchingData(UID):
+    # write the data first
+    writingData(UID)
+    # search inside index for entries containing 
+    with ix.searcher() as searcher:
+        print(UID)
+        for tag in tags:
+            query = QueryParser('content', ix.schema).parse(tag)
+            results = searcher.search(query, limit=
+                                      None, terms=True)
+            print(results[1:2])
+            for hit in results:
+                print(hit.matched_terms())
+                # print(hit["content"])
+    print('done searching')
 
